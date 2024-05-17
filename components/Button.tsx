8e3c6f7e37f4957af7c2ff2a9662a129eff56fd0
fauzanaslam <fauzanaslamdhani@gmail.com>
2024-05-17 11:44:54 +0700

    done

--- components/Button.tsx
+++ components/Button.tsx
@@ -5,13 +5,16 @@ type ParamsProps = {
   title: string;
   icon?: string;
   variant: string;
+  full?: Boolean;
 };
 
-const Button = ({ type, title, icon, variant }: ParamsProps) => {
+const Button = ({ type, title, icon, variant, full }: ParamsProps) => {
   return (
     <button
       type={type}
-      className={`flexCenter gap-3 rounded-full border ${variant}`}
+      className={`flexCenter gap-3 rounded-full border ${variant} ${
+        full && "w-full"
+      }`}
     >
       {icon && <Image src={icon} alt={title} width={24} height={24} />}
       <label className="font-semibold whitespace-nowrap">{title}</label>
